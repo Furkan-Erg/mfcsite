20ff0c3d240da944b9ad2ce41d68b8bbde4f83fb
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-04-13 22:50:13 +0300

    animation on scroll handled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about/about.component';
-
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AnimateOnScrollModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
